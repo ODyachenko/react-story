aef7a1e38eb56b49e718fefc8e23395bbe630fcb
ODyachenko <oleg1696@gmail.com>
2023-10-11 16:53:44 +0300

    book cover

---
+++ src/components/BookCover/BookCover.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const BookCover = React.forwardRef((props: any, ref: any) => {
+  return <div className="page page-cover" ref={ref} data-density="hard"></div>;
+});
