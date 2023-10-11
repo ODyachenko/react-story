02ed8d0f783d6ebd6ef31b7ee1d3c41798de8898
ODyachenko <oleg1696@gmail.com>
2023-10-11 16:52:59 +0300

    book navigation

---
+++ src/components/BookNavigation/BookNavigation.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { Btn } from '../../UI/Btn/Btn';
+import './styles.scss';
+
+type BookNavigationProps = {
+  onClickNext: () => void;
+  onClickPrev: () => void;
+};
+
+export const BookNavigation: FC<BookNavigationProps> = ({
+  onClickNext,
+  onClickPrev,
+}) => {
+  return (
+    <div className="book__navigation">
+      <Btn direction="left" onClick={onClickPrev} />
+      <Btn direction="right" onClick={onClickNext} />
+    </div>
+  );
+};
